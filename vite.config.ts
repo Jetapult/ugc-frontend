e210a8b6d4d65663c0747643aa396c84ace05b7b
Rushikesh Suryawanshi <rushikesh@fastcode.ai>
2025-07-30 18:47:37 +0530

    Allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001,
+    host: true,
+    // @ts-ignore - allowedHosts is supported by Vite but missing in type defs
+    allowedHosts: ["ugcf.vadapav.art"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
